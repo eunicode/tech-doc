fdeea20ac05a044161f3fb247f4a32360751cb16
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-09-11 23:52:50 -0700

    Package AJAX request into function

--- src/js/main.js
+++ src/js/main.js
@@ -17,51 +17,59 @@
 // Method #2: INSERT SCRIPT TAG INTO THE DOM (AJAX technique)
 // The script will download and execute immediately. But it is still difficult to manage your dependencies. You can never guarantee that multiple scripts will load and execute in the order you need them.
 
-const script = document.createElement("script");
-script.src = "data/data.js";
-document.head.appendChild(script);
+// const script = document.createElement("script");
+// script.src = "data/data.js";
+// document.head.appendChild(script);
 
-const DOMNode = document.querySelector('#main-doc');
+// const DOMNode = document.querySelector('#main-doc');
 
-data.map(x => {
-    const listItem = document.createElement('div');
-    listItem.className = 'main-section';
-    listItem.innerHTML = `<h4>${x.title}</h4>
-    <p>${x.info}</p>`;
-    DOMNode.appendChild(listItem);
-});
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+// data.map(x => {
+//     const listItem = document.createElement('div');
+//     listItem.className = 'main-section';
+//     listItem.innerHTML = `<h4>${x.title}</h4>
+//     <p>${x.info}</p>`;
+//     DOMNode.appendChild(listItem);
+// });
 
 // Method #3: AJAX + JSON METHOD
 // Store data in JS object so I can use template literals. The pro using of template literals is that any newline characters inserted in the source are part of the template literal. In other words, it's easier to write multi-line strings.
 // Then convert JS object to a JSON string with JSON.stringify() method. This is bc when exchanging data between a browser and a server, the data can only be text/blob/etc. When we receive JSON data from the server, we can easily convert JSON data to JS objects.
 
-// let dataA;
+function loadData(url) {
+    let dataA;
+    const DOMNode = document.querySelector('#main-doc');
 
-// const DOMNode = document.querySelector('#main-doc');
+    const request = new XMLHttpRequest;
 
-// const request = new XMLHttpRequest;
+    request.open('GET', url);
 
-// request.open('GET', '../data/data.json');
+    request.onreadystatechange = () => {
+        if (request.status === 200 && request.readyState === 4) {
+            dataA = JSON.parse(request.responseText);
+            console.log({ dataA });
+            for (const item in dataA.key) {
+                if (dataA.key.hasOwnProperty(item)) {
+                    const element = dataA.key[item];
+                    const listItem = document.createElement('div');
+                    listItem.className = 'main-section';
+                    listItem.innerHTML = `<h4>${element.title}</h4>
+                    <p>${element.info}</p>`;
+                    DOMNode.appendChild(listItem);
+                }
+            }
+        }
+    }
 
-// request.onreadystatechange = () => {
-//     if (request.status === 200 && request.readyState === 4) {
-//         dataA = JSON.parse(request.responseText);
-//         console.log({ dataA });
-//         for (const item in dataA.key) {
-//             if (dataA.key.hasOwnProperty(item)) {
-//                 const element = dataA.key[item];
-//                 const listItem = document.createElement('div');
-//                 listItem.className = 'main-section';
-//                 listItem.innerHTML = `<h4>${element.title}</h4>
-//                 <p>${element.info}</p>`;
-//                 DOMNode.appendChild(listItem);
-//             }
-//         }
-//     }
-// }
+    request.send();
+}
 
-// request.send();
+loadData('../data/data.json');
 
 // TO DO
-// Convert XHR to fetch
 // Use promises
+// Convert XHR to fetch
+// Async/await
