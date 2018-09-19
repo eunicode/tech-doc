adea67fdb931283e933b7cb4bb7c8da6810e21c2
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-09-18 23:24:59 -0700

    :recycle: Switch from XHR API to Fetch API

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Importing data from another file
+
 // Method #1: SCRIPT TAG METHOD
 
 // Use JS code in another file by adding script tag in HTML in the correct order
@@ -17,7 +19,7 @@
 
 /* ========================================================================== */
 
-// Method #2: INSERT SCRIPT TAG INTO THE DOM (AJAX technique)
+// Method #2: INSERT SCRIPT TAG INTO THE DOM (Did not work)
 
 // The script will download and execute immediately. But it is still difficult to manage your 
 // dependencies. You can never guarantee that multiple scripts will load and execute in the order 
@@ -43,7 +45,10 @@
 
 /* ========================================================================== */
 
-// Method #3: AJAX + JSON METHOD
+// Method #3.1: AJAX (XHR) + JSON
+// Method #3.2: AJAX + JSON + PROMISE
+
+// See code with comments here: https://github.com/eunicode/tech-doc/blob/30adf4fd44e9a27ae518f32b44400e9aa19c44c0/src/js/main.js
 
 // Store data in JS object so I can use template literals. The pro using of template literals is that 
 // any newline characters inserted in the source are part of the template literal. In other words, 
@@ -52,48 +57,30 @@
 // data between a browser and a server, the data can only be text/blob/etc. When we receive JSON data 
 // from the server, we can easily convert JSON data to JS objects.
 
+/*
 function getJSON(url) {
-  
-    // Create Promise object
-    return new Promise( 
-  
-        // Executor function with `resolve` function passed as an argument 
-        function(resolve, reject) {
-  
-            // Create XMLHttpRequest Object
+    return new Promise(
+        function (resolve, reject) {
             const request = new XMLHttpRequest();
-            
-            // Open request
             request.open('GET', url);
-             
-            // AJAX event callback function
-            // Alternative on-event handler: onreadystatechange + if readyState done + if status code 200
             request.onload = () => {
                 if (request.status === 200) {
-                    // Call `resolve` callback function once asynchronous work completes
-                    // `resolve`'s first argument is the value the promise will become if the promise 
-                    // is fulfilled
                     resolve(JSON.parse(request.responseText));
                 } else {
-                    reject( `${Error('Couldn\'t load data. HTTP response status: ')} ${request.statusText}` );
+                    reject(`${Error('Couldn\'t load data. HTTP response status: ')} ${request.statusText}`);
                 }
             }
-  
-            // Send request
-            request.send(); 
-  
+            request.send();
+
         }
-  
     );
-  
-  }
-  
-  // First `then` onFulfillment callback
-  function generateListSections(data) {
+}
+
+function generateListSections(data) {
     console.log({ data });
-  
+
     const DOMNode = document.querySelector('#main-doc');
-    
+
     for (const item in data.key) {
         if (data.key.hasOwnProperty(item)) {
             const element = data.key[item];
@@ -101,62 +88,99 @@ function getJSON(url) {
             listSection.className = 'main-section';
             listSection.id = createCssId(element.title);
             listSection.innerHTML = `<header><h2>${element.title}</h2></header>
+                                <p>${element.info}</p>
+                                <code>${element.code}</code>`;
+            DOMNode.appendChild(listSection);
+        }
+    }
+
+    return 'Testing chaining :)'
+}
+
+function createCssId(title) {
+    return title.replace(/ /g, '_');
+}
+
+function testChaining(string) {
+    console.log(`Chaining was successful if you see this string: '${string}'`);
+    throw 'Uh-oh :(';
+}
+
+console.log(getJSON('../data/data.json'));
+
+getJSON('../data/data.json')
+    .then(generateListSections)
+    .then(testChaining)
+    .catch(error => console.log(error));
+*/
+
+/* ========================================================================== */
+  
+// METHOD #4 FETCH + JSON
+
+function generateListSections(data) {
+    console.log('This is the fetched, parsed data that was passed down: ', data);
+  
+    const DOMNode = document.querySelector('#main-doc');
+    
+    for (const item in data.key) {
+        if (data.key.hasOwnProperty(item)) {
+            const element = data.key[item];
+            const listSection = document.createElement('section');
+
+            listSection.className = 'main-section';
+            listSection.id = createCssId(element.title);
+
+            listSection.innerHTML = `<header><h2>${element.title}</h2></header>
                                   <p>${element.info}</p>
                                   <code>${element.code}</code>`;
+
             DOMNode.appendChild(listSection);
         }
     }
   
     return 'Testing chaining :)'
     // throw 'Uh-oh!';
-  }
+}
   
-  function createCssId(title) {
+function createCssId(title) {
     return title.replace(/ /g , '_');
-  }
-  
-  function testChaining(string) {
+}
+
+function testChaining(string) {
     console.log(`Chaining was successful if you see this string: '${string}'`);
     throw 'Uh-oh :(';
-  }
-  
-  // See first Promise object
-  // {
-  //     __proto__: Promise,
-  //     [[PromiseStatus]]: "resolved",
-  //     [[PromiseValue]]: { key: [...] }
-  // }
-  console.log(getJSON('../data/data.json'));
-  
-  // See Promise object returned by `then` - no arguments
-  // If both arguments are omitted from `then`, then when the Promise that `then` is called on adopts
-  // the state fulfilled, a new Promise is created that is identical to the Promise on which `then`
-  // was called.
-  // console.log(getJSON('../data/data.json').then()); // { closure: ..., data: ...}
-  
-  // See Promise object returned by `then` - `onFulfilled` returns a value
-  // console.log(getJSON('../data/data.json').then(function(data) { return 6; })); // 6
-  
-  // See Promise object returned by `then` - `onFulfilled` returns nothing
-  // console.log(getJSON('../data/data.json').then(function(data) { 1 + 2; })); // undefined 
-  
-  getJSON('../data/data.json')
-    //  The `then` method returns a Promise in the pending status.
-    // The first argument of `then` is the `onFulfilled` function
-    // The `onFulfilled` function has one argument, the fulfillment value
-    // `onFulfilled` handler gets called asynchronously, as soon as the stack is empty.
-    // If `onFulfilled` returns a value, the promise gets resolved with that value
-    // If `onFulfilled` returns nothing, the promise gets resolved with `undefined`
+}
+
+// Create a Request object using Request() constructor 
+// Second parameter is `init`, an options object
+const getJSON = new Request('../data/data.json', {
+    method: 'GET'
+});
+
+// See Promise object that fetch returns
+console.log(fetch(getJSON));
+
+// Create fetch request. Fetch Request object with fetch() call.
+// First parameter can be a URL or Request object
+fetch(getJSON)
+    // `fetch` returns a promise that resolves to the Response object of the 
+    // resource request.
+    // Since `fetch` returns a promise, we can use `then` method.
+    // We pass the Response object to then's onFulfillment callback function
+    // We run Body.json() on the response to parse it (body text) as JSON.
+    .then( (response) => response.json() )
     .then(generateListSections)
     .then(testChaining)
-    .catch( e => console.log(e) );
-  
-  // TO DO
-  // Use promises ✓
-  // Convert XHR to fetch ✓
-  // Async/await
-  
-  // Change anonymous functions to named functions
-  // Change function declarations to arrow functions
-  // Refactor onreadystatechange handler to use `this` to refer to XHR object
-  // Refactor object in data.json to array
+    .catch( error => console.log(error) );
+
+// TO DO
+// Use promises ✓
+// Convert XHR to fetch ✓
+// Async/await
+
+// Change anonymous functions to named functions
+// Change function declarations to arrow functions
+// Refactor onreadystatechange handler to use `this` to refer to XHR object
+// Refactor object in data.json to array
+// Switch from using innerHTML + HTML text to building DOM elements
